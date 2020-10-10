a8eb65cda4ade88695843943cd71781029b4525c
soulyana <soulyana.lakew@accenturefederal.com>
2020-10-09 21:17:55 -0400

    request headers

--- web_scraping/HTTP_requests/search.py
+++ web_scraping/HTTP_requests/search.py
@@ -4,11 +4,17 @@ import requests
 response = requests.get(
     'https://api.github.com/search/repositories',
     #params={'q': 'requests+language:python'},
-    params={'q': 'schedule+user:dbader'}
+    params={'q': 'HTTP for Humans+language:python'},
+    headers={'Accept': 'application/vnd.github.v3.text-match+json'},
 )
+
+# View the new 'text-matches array which provides information about
+# your search terms within the results
+
 
 # Inspect some attributes of the repository
 json_response = response.json()
 repository = json_response['items'][0] #are you feeling lucky?
-print(f'Repository name: {repository["name"]}') # Python 3.6+
-print(f'Repository description: {repository["description"]}')
+# print(f'Repository name: {repository["name"]}') # Python 3.6+
+# print(f'Repository description: {repository["description"]}')
+print(f'Text matches: {repository["text_matches"]}')
